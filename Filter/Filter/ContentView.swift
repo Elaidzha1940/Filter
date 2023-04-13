8992219d2f0a15bd4d67845332dc9c6efe5ab62e
Elaidzha1940 <eli080293@gmail.com>
2023-04-13 16:48:09 +0700

    Filter / 13 + added text

--- Filter/Filter/ContentView.swift
+++ Filter/Filter/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Spacer()
+                
+                Text("Фильтры")
+                    .font(.title)
+                    .foregroundColor(.black)
+                
+                Spacer()
+            }
+            .padding()
+            
+            Spacer()
+            
         }
-        .padding()
     }
 }
 
