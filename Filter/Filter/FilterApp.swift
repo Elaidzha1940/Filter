f39dfbdf536fa59dfc92c00bd57de5c7107fb429
Elaidzha1940 <eli080293@gmail.com>
2023-04-13 16:42:43 +0700

    Filter / 13

---
+++ Filter/Filter/FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterApp.swift
+//  Filter
+//
+//  Created by Elaidzha Shchukin on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
