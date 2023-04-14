bd3d98072cfe15d11d24666e00e991d03ac5994d
Elaidzha1940 <eli080293@gmail.com>
2023-04-14 18:25:42 +0700

    added a New File

---
+++ Filter/Filter/UserProfile.swift
@@ -0,0 +1,24 @@
+//
+//  UserProfile.swift
+//  Filter
+//
+//  Created by Elaidzha Shchukin on 14.04.2023.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+      
+        VStack {
+            
+            
+        }
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
